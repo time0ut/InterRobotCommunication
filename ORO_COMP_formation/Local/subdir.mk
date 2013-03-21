dd45aa0faed7926d8603cdd5f00538cac0bdd723
VM 17 <>
2013-03-21 11:16:05 +0100

    * Eclipse project: less unused libs, compile linking with libpcre.a

--- ORO_COMP_formation/Local/subdir.mk
+++ ORO_COMP_formation/Local/subdir.mk
@@ -17,7 +17,7 @@ CPP_DEPS += \
 %.o: ../%.cpp
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C++ Compiler'
-	g++ -DOROCOS_TARGET=gnulinux -DOCL_DLL_EXPORT -I"/home/dmia/workspace/workspaceEclipse/ORO_TYPE_base-types" -I/home/dmia/InterRobotCommunication/local/include -I/opt/orocos-toolchain/install/include/orocos -I/opt/orocos-toolchain/install/include -I/usr/include/mrpt/bayes/include -I/usr/include/mrpt/obs/include -I/usr/include/mrpt/mrpt-config -I/usr/include/mrpt/hwdrivers/include -I/usr/include/mrpt/base/include -I/usr/include/mrpt/opengl/include -I/usr/include/mrpt/utils/include -I/usr/include/mrpt/vision/include -I/usr/include/mrpt/poses/include -I/usr/include/mrpt/slam/include -I/usr/include/mrpt/gui/include -I/usr/include/mrpt/maps/include -O3 -Wall -c -fmessage-length=0 -fPIC -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	g++ -DOROCOS_TARGET=gnulinux -DOCL_DLL_EXPORT -I"/home/dmia/workspace/workspaceEclipse/ORO_TYPE_base-types" -I/home/dmia/InterRobotCommunication/local/include -I/opt/orocos-toolchain/install/include/orocos -I/opt/orocos-toolchain/install/include -O3 -Wall -c -fmessage-length=0 -fPIC -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
