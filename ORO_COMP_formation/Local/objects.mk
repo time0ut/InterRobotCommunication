ae9f1e045dfc30ec31a48bcc95d78837ed38355c
dmia <dmia@dmia-vm>
2013-03-22 00:48:58 +0100

    lib2

--- ORO_COMP_formation/Local/objects.mk
+++ ORO_COMP_formation/Local/objects.mk
@@ -4,5 +4,5 @@
 
 USER_OBJS := /home/dmia/InterRobotCommunication/local/lib/ivy/libivy.a /home/dmia/InterRobotCommunication/local/lib/libpcre.a
 
-LIBS := -lmrpt-scanmatching -lORO_component_tools_Local -lmrpt-maps -lmrpt-obs -lmrpt-opengl -lmrpt-base -lmrpt-vision -lmrpt-slam -lmrpt-bayes
+LIBS := -lmrpt-scanmatching -lorocos-rtt-gnulinux -lORO_component_tools_Local -lmrpt-maps -lmrpt-obs -lmrpt-opengl -lmrpt-base -lmrpt-vision -lmrpt-slam -lmrpt-bayes
 
