80f9aa15369557febcccbb01b4ce273194ca16ca
dmia <dmia@dmia-VM_ET18.(none)>
2013-03-21 11:47:40 +0100

    * IvyLauncher

--- ORO_COMP_IvyLauncher/Gumstix/objects.mk
+++ ORO_COMP_IvyLauncher/Gumstix/objects.mk
@@ -4,5 +4,5 @@
 
 USER_OBJS := /home/dmia/InterRobotCommunication/gumstix/lib/ivy/libivy.a
 
-LIBS := -lmrpt-scanmatching -lmrpt-maps -lmrpt-obs -lmrpt-opengl -lmrpt-base -lmrpt-vision -lmrpt-slam -lmrpt-bayes
+LIBS :=
 
